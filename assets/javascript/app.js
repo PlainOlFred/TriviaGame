42c898585fc7fedab07c4d780d15311f1e162bd1
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-18 22:00:29 -0500

    created countDown function and readyGame functions. calle countDown in rady game.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,38 @@
-$('#gameButton').click(function() {
-    alert('me')
-})
+$(document).ready(function(){
 
-$('.ansOption').click(function(){
-    console.log('triva');
+    const questionBank = [[],[],[]]
+
+    //click to start
+    let gameStart = false;
+    const timer = new Array(11).fill(0).map((x,index) => index).reverse()
+    
+
+    let countDown = function(){
+        setInterval(()=>$('#clockButton').text(timer.shift()), 1000)
+        
+    }
+
+    let gameReady =function(){
+        gameStart=true
+        $(this).text('Ready...').unbind()
+        setTimeout(()=>{$('#clockButton').text('Set...')}, 1000)
+        setTimeout(()=>{$('#clockButton').text('Go!!!')}, 1750)
+        setTimeout(()=> countDown(), 2000)
+    }
+
+    // setInterval(()=>timer.forEach((x)=>{x;console.log(x)}), 1000)
+     
+     
+    
+    $('#clockButton').click(gameReady)
+
+    
+
+    $('.ansOption').click(function(){
+        console.log('triva');
+        
+    })
+    // setInterval(()=>console.log('hi'), 1000)
     
 })
-
-console.log('ho');
 
