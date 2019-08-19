1da6bb6f51c9cf482fca5d6e2b0fed86542b7022
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-18 22:46:54 -0500

    turned startGame button into a h4 tag, created a collection of question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
 
-    const questionBank = [[],[],[]]
+    const qusFact = function(qus, ans){
+        let q = qus;
+        let a = ans;
+
+        return{
+            qus:q,
+            ans:a,
+        }
+    }
+    
+    let qus1 = qusFact('What is best band','rush')
+    let qus2 = qusFact('whos saw it', 'me' )
+    
+    console.log(qus1.qus)
+    console.log(qus1.ans)
+    console.log(qus2.qus)
+    console.log(qus2.ans)
+
+
 
     //click to start
     let gameStart = false;
-    const timer = new Array(11).fill(0).map((x,index) => index).reverse()
+    const timer = new Array(101).fill(0).map((x,index) => index).reverse()
     
 
     let countDown = function(){
@@ -14,9 +32,9 @@ $(document).ready(function(){
 
     let gameReady =function(){
         gameStart=true
-        $(this).text('Ready...').unbind()
-        setTimeout(()=>{$('#clockButton').text('Set...')}, 1000)
-        setTimeout(()=>{$('#clockButton').text('Go!!!')}, 1750)
+        $(this).html('<h4>Ready...</h4>').unbind()
+        setTimeout(()=>{$('#clockButton').html('<h4>Set...<h4>')}, 1000)
+        setTimeout(()=>{$('#clockButton').html('<h4>Go!!!</h4>')}, 1750)
         setTimeout(()=> countDown(), 2000)
     }
 
