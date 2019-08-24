76b710ee63d0c08c0e2af06b01622049577aa051
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-24 11:32:27 -0500

    styled anser options, function one Round Modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+   
 
     const qusFact = function(qus, ans, wro1, wro2, wro3){
         let q = qus;
@@ -18,48 +19,50 @@ $(document).ready(function(){
     
     let qus1 = qusFact('What is best band','Rush','not rush1', 'not rush2', 'not rush3')
     let qus2 = qusFact('whos saw it', 'Me' , 'not me1', 'not me2' , 'not me3')
+    let qus3 = qusFact('what color is the sky', 'blue', 'red', 'red', 'red')
+    let qus4 = qusFact('What is next?', 'This One', 'That one', 'That One', 'That One') 
 
-    qusBank = [qus1, qus2]
+    const qusBank = [qus1, qus2, qus3, qus4]
 
 
-    let gameStart=false;;
-    const timer = new Array(30).fill(0).map((x,index) => index).reverse()
-    
-
+    let gameStart=false;
+    //timer
+    const timer = new Array(10).fill(0).map((x,index) => index).reverse()
     let countDown = function(){
         setInterval(()=>$('#clockButton').text(timer.shift()), 1000)
         
     }
 
     let gameReady =function(){
-        $(this).html('<h4>Ready...</h4>').unbind(); console.log('this :' +this)
+        $(this).html('<h4>Ready...</h4>').unbind(); 
         setTimeout(()=>{$('#clockButton').html('<h4>Set...<h4>')}, 1000)
         setTimeout(()=>{$('#clockButton').html('<h4>Go!!!</h4>')}, 1750)
         setTimeout(()=> oneRound(), 2000)
         gameStart=true;  
     }
 
+    //end of roud Modals
+    function oneRoundModal(){
+        $('.ansOption').attr('disabled','disabled')
+
+    }
+    
+
     let oneRound= function(){
         countDown()
         rnQus = Math.floor(Math.random()*qusBank.length)//random number for the question number
-        setTimeout(()=>{$('#qusAnsText').html(`<h1>${qusBank[rnQus].qus}</h1`)}, 250)
+        setTimeout(()=>{$('#qusAnsText').html(`<h1>${qusBank[rnQus].qus}</h1`)}, 500)
+        
+        let rdArr = [qusBank[rnQus].ans, qusBank[rnQus].wro1, qusBank[rnQus].wro2, qusBank[rnQus].wro3]
+        let rdAns = rdArr[0]//this round answer
         
         
         //randominize
-        let rdArr = [qusBank[rnQus].ans, qusBank[rnQus].wro1, qusBank[rnQus].wro2, qusBank[rnQus].wro3]
-        let rdAns = rdArr[0]
         let rnQusArr=[]; 
+
         do{
             rnQusArr.push(rdArr.splice(Math.floor(Math.random()*rdArr.length),1))
-
         } while(rdArr.length >0)
-
-        
-        console.log(rnQusArr);
-        qusBank.splice(qusBank[rnQus], 1)//removes current question from qusBank
-        console.log('After splice' +qusBank[1]);
-        
-        
 
         setTimeout(()=>{$('#ansOptions1').text(rnQusArr[0])}, 500) 
         setTimeout(()=>{$('#ansOptions2').text(rnQusArr[2])}, 500)
@@ -67,14 +70,20 @@ $(document).ready(function(){
         setTimeout(()=>{$('#ansOptions4').text(rnQusArr[3])}, 500)
 
         $('.ansOption').click(function(){
+            
             let userGuess = $(this).text()
             if(userGuess===rdAns){
                 setTimeout(()=>{$('#qusAnsText').html(`<h1>Correct</h1`)}, 250)
             }else
             setTimeout(()=>{$('#qusAnsText').html(`<h1>Wrong. Wrong. Wrong: Correct Answer is ${rdAns}</h1>` )}, 250)
+            
+
+            qusBank.splice(rnQus,1)
+
+            oneRoundModal()
 
         })
-        //end of roud Modals
+       
         
         
             
@@ -97,4 +106,3 @@ $(document).ready(function(){
     // setInterval(()=>console.log('hi'), 1000)
     
 })
-
