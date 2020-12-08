c62e1d848f8127fb92de66abf4d5cddb15ba92f4
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-12-07 22:43:28 -0600

    Moved questions into separate js file

---
+++ assets/javascript/questions.js
@@ -0,0 +1,70 @@
+let qus1 = qusFact(
+    "Another name for Loop 343?",
+    "Lamar",
+    "Mopac",
+    "The Drag",
+    "Research Blvd"
+  );
+  let qus2 = qusFact(
+    "What does Mopac stand for?",
+    "Missouri Pacific",
+    "Motor Primary Accesess",
+    " MOP Access Corridor",
+    "MoPac"
+  );
+  let qus3 = qusFact(
+    "Which road runs East/West?",
+    "Barton Springs",
+    "Congress",
+    "1st Street",
+    "Red River"
+  );
+  let qus4 = qusFact(
+    "Which is not a Bar District?",
+    "Zilker Park",
+    "Sixth Street",
+    "Rainey Street",
+    "Rock Rose"
+  );
+  let qus5 = qusFact(
+    "Where is ACL held",
+    "Zilker Park",
+    "Sixth Street",
+    "Auditorium Shores",
+    "Rock Rose"
+  );
+  let qus6 = qusFact(
+    "Where does the UT basketball team player?",
+    "Frank Erwin Center",
+    "UT Arena",
+    "Circuits of the America",
+    "Austin High School"
+  );
+  let qus7 = qusFact(
+    "What road leads up to the State Capitol?",
+    "Congress",
+    "Lamar",
+    "1st street",
+    "Sixth Street"
+  );
+  let qus8 = qusFact(
+    "What is the InterState highway that passing through Austin?",
+    "I-35",
+    "I-290",
+    "I-45",
+    "I-130"
+  );
+  let qus9 = qusFact(
+    "Tallest building in Austin?",
+    "The Independent",
+    "Frost Tower",
+    "Austonian",
+    "Fairmont"
+  );
+  let qus10 = qusFact(
+    "Which animal live under the Congress bridge?",
+    "Bats",
+    "Cats",
+    "Dogs",
+    "Crows"
+  );
