165c58deb69a765b9c47a1598b72c6b463176efe
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-12-09 20:06:55 -0600

    Fixed question object property name

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,51 +1,51 @@
 const questions = [
   {
-    qusetion: "Another name for Loop 343?",
+    question: "Another name for Loop 343?",
     answer: "Lamar",
     choices: ["Mopac", "The Drag", "Research Blvd"],
   },
   {
-    qusetion: "What does Mopac stand for?",
+    question: "What does Mopac stand for?",
     answer: "Missouri Pacific",
     choices: ["Motor Primary Accesess", " MOP Access Corridor", "MoPac"],
   },
   {
-    qusetion: "Which road runs East/West?",
+    question: "Which road runs East/West?",
     answer: "Barton Springs",
     choices: ["Congress", "1st Street", "Red River"],
   },
   {
-    qusetion: "Which is not a Bar District??",
+    question: "Which is not a Bar District??",
     answer: "Zilker Park",
     choices: ["Sixth Street", "Rainey Street", "Rock Rose"],
   },
   {
-    qusetion: "Where is ACL held?",
+    question: "Where is ACL held?",
     answer: "Zilker Park",
     choices: ["Sixth Street", "Auditorium Shores", "Rock Rose"],
   },
   {
-    qusetion: "Where does the UT basketball team player?",
+    question: "Where does the UT basketball team player?",
     answer: "Frank Erwin Center",
     choices: ["UT Arena", "Circuits of the America", "Austin High School"],
   },
   {
-    qusetion: "What road leads up to the State Capitol?",
+    question: "What road leads up to the State Capitol?",
     answer: "Congress",
     choices: ["Lamar", "1st street", "Sixth Street"],
   },
   {
-    qusetion: "What is the InterState highway that passing through Austin?",
+    question: "What is the InterState highway that passing through Austin?",
     answer: "I-35",
     choices: ["I-290", "I-45", "I-130"],
   },
   {
-    qusetion: "Tallest building in Austin?",
+    question: "Tallest building in Austin?",
     answer: "The Independent",
     choices: ["Frost Tower", "Austonian", "Fairmont"],
   },
   {
-    qusetion: "Which animal live under the Congress bridge?",
+    question: "Which animal live under the Congress bridge?",
     answer: "Bats",
     choices: ["Cats", "Dogs", "Crows"],
   },
